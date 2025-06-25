6702c7f5232094266264aae15c59bce31ade93ca
mchia101 <mchia@logitech.com>
2025-06-25 18:57:13 +0800

    1. writer.sh function complete

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -7,4 +7,21 @@ set -e
 #stop while executing the script if any variable is not set
 set -u
 
+if [ $# -lt 2 ]
+then
+    echo "Usage: $0 <writefile> <string>"
+    exit 1
+else
+    WRITEFILE=$1
+    WRITE_STRING=$2
+fi
 
+if ! mkdir -p "$(dirname "$WRITEFILE")" >/dev/null 2>&1 || ! touch "$WRITEFILE" >/dev/null 2>&1; 
+then
+    #  Print an error message and exit with status 1
+    echo "File $WRITEFILE could not be created."
+    exit 1
+else
+    echo "File $WRITEFILE created successfully."
+    echo "$WRITE_STRING" > "$WRITEFILE"
+fi
