7336e03fb4834047abbb47465e07433660956c4c
mchia101 <mchia@logitech.com>
2025-06-25 17:45:18 +0800

    1. finder.sh function ready 2. add writer.sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# assigment 1 for finder.sh
+# Author: mchia101
+
+# stop while executing the script if any command fails
+set -e
+#stop while executing the script if any variable is not set
+set -u
+
+
